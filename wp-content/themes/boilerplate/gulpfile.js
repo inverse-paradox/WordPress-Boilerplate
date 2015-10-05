0fe4685be6c00e9d4da5becadc332a9af6d05c0d
Josh Hannan <josh@inverseparadox.net>
2015-10-05 12:15:55 -0400

    added gulp to boilerplate, added gitignore

--- wp-content/themes/boilerplate/gulpfile.js
+++ wp-content/themes/boilerplate/gulpfile.js
@@ -1,99 +1,52 @@
-var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    sftp = require('gulp-sftp'),
-    //livereload = require('gulp-livereload'),
-    del = require('del');
+// Include gulp
+var gulp = require('gulp');
 
-// var host = '45.33.118.223',
-//     user = 'delucahomes',
-//     pass = 'j)%Rn.5HHH@H8X}2',
-//     port = '2222',
-//     path = '/test/';
+// Include Our Plugins
+var sass = require( 'gulp-sass' );
+var imagemin = require( 'gulp-imagemin' );
+var pngquant = require( 'imagemin-pngquant' );
+var concat = require( 'gulp-concat' );
+var uglify = require( 'gulp-uglify' );
+var rename = require( 'gulp-rename' );
+var sourcemaps = require( 'gulp-sourcemaps' );
 
-
-// Styles
-gulp.task('styles', function() {
-  return sass('scss/global.scss', { style: 'expanded' })
-    .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest('css'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(minifycss())
-    .pipe(gulp.dest('css'))
-    .pipe(notify({ message: 'Styles task complete' }));
-    // .pipe(sftp({
-    // 	host: host,
-    // 	user: user,
-    // 	pass: pass,
-    // 	port: port,
-    // 	remotePath: path+'/css/'
-    // }))
-    //.pipe(livereload());
+// Imagemin Task
+gulp.task( 'imagemin', function() {
+	return gulp.src( ['images/**/*.jpg', 'images/**/*.png', 'images/**/*.gif', '!images/min/**/*'] )
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))
+		.pipe( gulp.dest( './images/min' ) );
 });
 
-// // Scripts
-// gulp.task('scripts', function() {
-//   return gulp.src('src/scripts/**/*.js')
-//     .pipe(jshint('.jshintrc'))
-//     .pipe(jshint.reporter('default'))
-//     .pipe(concat('main.js'))
-//     .pipe(gulp.dest('dist/scripts'))
-//     .pipe(rename({ suffix: '.min' }))
-//     .pipe(uglify())
-//     .pipe(gulp.dest('dist/scripts'))
-//     .pipe(notify({ message: 'Scripts task complete' }));
-// });
-
-// Images
-gulp.task('images', function() {
-  return gulp.src('images/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('images'))
-    .pipe(notify({ message: 'Images task complete' }))
-    // .pipe(sftp({
-    // 	host: host,
-    // 	user: user,
-    // 	pass: pass,
-    // 	port: port,
-    // 	remotePath: path
-    // }))
-    ;
+// Compile Sass
+gulp.task( 'sass', function() {
+	return gulp.src('scss/**/*.scss')
+		.pipe( sourcemaps.init( { loadMaps: true } ) )
+		.pipe( sass.sync({ outputStyle : 'compressed' }).on('error', sass.logError) )
+		.pipe( sourcemaps.write( '../css' ) )
+		.pipe(gulp.dest('css'));
 });
 
-// Clean
-gulp.task('clean', function(cb) {
-    del(['assets/css', 'assets/js', 'assets/img'], cb)
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+	return gulp.src( ['js/lib/jquery.cycle2.min.js', 'js/vendor/*.js' ])
+		.pipe( concat('theme.js') )
+		.pipe( sourcemaps.init( { loadMaps: true } ) )
+		.pipe( rename('theme.min.js') )
+		.pipe( uglify() )
+		.pipe( sourcemaps.write( '../js' ) )
+		.pipe( gulp.dest('js') );
 });
 
-// Default task
-gulp.task('default', ['clean'], function() {
-    gulp.start('styles', 'images');
+// Watch Files For Changes
+gulp.task( 'watch', function() {
+	gulp.watch( ['js/vendor/**/*.js'], [ 'scripts'] );
+	gulp.watch( ['scss/**/*.scss'], ['sass'] );
+	gulp.watch( ['images/**/*', '!images/min/**/*'], ['imagemin'] );
 });
 
-// Watch
-gulp.task('watch', function() {
-
-  // Create LiveReload server
-  //livereload.listen();
-
-  // Watch .scss files
-  gulp.watch('scss/**/*.scss', ['styles']);
-
-  // Watch .js files
-  //gulp.watch('js/*.js', ['scripts']);
-
-  // Watch image files
-  gulp.watch('images/*', ['images']);
-
-  // Watch any files in dist/, reload on change
-  //gulp.watch(['css/**']).on('change', livereload.changed);
-
-});
+// Default Task
+gulp.task( 'default', [ 'imagemin', 'sass', 'scripts', 'watch'] );
