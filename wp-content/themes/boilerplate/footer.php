370515abca8bc6515cf95baeedfa2555cdb85b39
Jessica DiPonziano <jess@inverseparadox.net>
2018-03-05 11:04:55 -0500

    Update footer

--- wp-content/themes/boilerplate/footer.php
+++ wp-content/themes/boilerplate/footer.php
@@ -4,13 +4,15 @@
  */
 ?>
 
-    <footer class="footer" role="contentinfo">
-        <div class="content-wrapper">
-            
-        </div><!--/content-wrapper-->
-    </footer><!--/footer-->
+        </div><!--END content-wrap-->
+    </div><!--END container-->
+</div><!--END page-wrap-->
+
+<footer class="footer" role="contentinfo">
+    <div class="container">
         
-</div><!--/page-wrapper-->
+    </div><!--END container-->
+</footer><!--END footer-->
 
 <?php wp_footer(); ?>
 </body>
