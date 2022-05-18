bf6960eecbed098a3481d8a93ec2de1fed9ca219
Erik Teichmann <erik@inverseparadox.net>
2022-05-18 09:30:12 -0400

    Adjusts styling in pattern

--- wp-content/themes/ip_master/patterns/sample-block-pattern.php
+++ wp-content/themes/ip_master/patterns/sample-block-pattern.php
@@ -5,8 +5,8 @@
  * Categories: text
  */
 ?>
-<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|tertiary"}}},"spacing":{"padding":{"top":"100px","right":"100px","bottom":"100px","left":"100px"}}},"backgroundColor":"vivid-red","textColor":"background","layout":{"inherit":false}} -->
-<div class="wp-block-group alignfull has-background-color has-vivid-red-background-color has-text-color has-background has-link-color" style="padding-top:100px;padding-right:100px;padding-bottom:100px;padding-left:100px"><!-- wp:heading -->
+<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|tertiary"}}},"spacing":{"padding":{"top":"100px","right":"100px","bottom":"100px","left":"100px"}}},"backgroundColor":"vivid-red","textColor":"white","layout":{"inherit":false}} -->
+<div class="wp-block-group alignfull has-white-color has-vivid-red-background-color has-text-color has-background has-link-color" style="padding-top:100px;padding-right:100px;padding-bottom:100px;padding-left:100px"><!-- wp:heading -->
 <h2>This is a sample block pattern</h2>
 <!-- /wp:heading -->
 
